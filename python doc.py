9acb7fb2af3b24558153b2280f8e7167317f7e83
athulyaa022 <athulyaa2028@cs.sjcetpalai.ac.in>
2024-10-04 16:05:33 +0530

    Add files via upload

---
+++ python doc.py
@@ -0,0 +1,57 @@
+#print("Hello World")
+#print("Hello\nWorld")
+
+'''
+if 5>2:
+    print("Five is greater than two")
+if 5>2:
+    print("Five is greater than two")
+if 5>2:
+        print("Five is greater than two")
+
+if 5>2:
+ print("Five is greater than two")
+ print("Five is greater than two")
+
+x=5
+x="Diya"
+print(x)
+
+
+x=str(5)
+y=int(5)
+z=float(5)
+print(x)
+print(y)
+print(z)
+
+x=5j
+y="John"
+print(type(x))
+print(type(y))
+
+x,y,z="Orange","Banana","Cherry"
+
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+
+x="Python "
+y="is "
+z="awesome"
+print(x+y+z)
+
+username=input("Enter your name:")
+print("Username is: " + username)
+
+a="Hello"
+b=" World"
+c=a+b
+print(c)
+
+a="Hello World"
+print(a[3])
+'''
+a="Hello World"
+print(a[:5])
