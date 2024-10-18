2094fde9b5be93848a100832d0ff5b0932fc178c
athulyaa022 <athulyaa2028@cs.sjcetpalai.ac.in>
2024-10-18 14:34:52 +0530

    Add files via upload

---
+++ Basiciprogram.py
@@ -0,0 +1,9 @@
+'''
+Author Name: Athulya A
+Description:program that asks the user to input their name and age.
+ After receiving the input, display a message that includes the user's name and age.
+Hello, John! You are 25 years old.
+'''
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print("Hello",name,"! You are",age,"years old")
