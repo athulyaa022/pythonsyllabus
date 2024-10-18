0dfb57ad1e5c9c9b70544d308bac82b21bcfd985
athulyaa022 <athulyaa2028@cs.sjcetpalai.ac.in>
2024-10-18 15:08:22 +0530

    Add files via upload

---
+++ Basiciprogram2.py
@@ -0,0 +1,10 @@
+'''
+Author Name: Athulya A
+Description:program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string.
+Finally, display the new string.
+'''
+
+str_var="Hello, World!"
+str_another_var=str_var[7:12]
+final_str=str_another_var + " Everyone!"
+print(final_str)
