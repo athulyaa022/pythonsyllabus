0ded2ea8852e27e05ce18adcabcb8f94685381c7
athulyaa022 <athulyaa2028@cs.sjcetpalai.ac.in>
2024-10-18 15:56:52 +0530

    Add files via upload

---
+++ arithematicop.py
@@ -0,0 +1,14 @@
+'''
+Author Name: Athulya A
+Date: 18-10-2024
+Description: Program that demonstrates the usage of arithmetic, comparison, and logical operators
+'''
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+num_sum=num1+num2
+num_division=num1/num2
+print("Sum:",num_sum,",","Division:",num_division)
+print("Is number1 grater than number2 ?",num1>num2)
+print("Are number1 and number2 equal ?",num1==num2)
+print("Logical AND:",num1>0 and num2>0)
+print("Logical OR:",num1<0 or num2>0)
